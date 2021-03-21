4c320d5036116dc5a34c0ee84eabcbcc6ca84d77
Anonteam798 <76845864+Anonteam798@users.noreply.github.com>
2021-03-21 18:06:57 -0500

    Update instalador.sh

--- instalador.sh
+++ instalador.sh
@@ -5,10 +5,14 @@ cd $HOME/Scripts
 cp recoveri.db $HOME
 
 rm Adm.py
-rm Readme.md
+rm README.md
+cd .. 
+sleep 1 
 
+rm -rf Scripts
 git clone https://github.com/Anonteam798/Scripts.git 
 
+sleep 1
 cp $HOME/recoveri.db $HOME/Scripts
 
 echo "Done Succefully "
