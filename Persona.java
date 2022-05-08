e565a4fc537d1447b11f64480ff1423935ef23cf
Anonteam798 <luisfm976@gmail.com>
2022-05-07 21:52:25 -0500

    En la clase Persona se implementa un método para ejecutar comandos facilmente

--- Persona.java
+++ Persona.java
@@ -8,21 +8,19 @@ import javax.swing.JOptionPane;
 public class Persona{
     public static void main(String[] args) {
         System.out.println("Hola mundo desde java");
-        try (FileOutputStream fos = new FileOutputStream(
-            new File("procesos.txt"))) {
-            String listarData = "ls";
-            String[] cmdWithParam  = {"ls","-al"};
-            Process prod =  Runtime.getRuntime().exec("pwd");
-            BufferedInputStream buffer = 
-            new BufferedInputStream( prod.getInputStream());
-            //fos.write(buffer.readAllBytes());
-            //JOptionPane.showMessageDialog(null, 
-            //"Proceso guardado con exito", "Root", JOptionPane.INFORMATION_MESSAGE);
-            System.out.println(new String(buffer.readAllBytes()));
+        ejecutarCommando("java --version");
+        
+    }
 
-            
+    static void ejecutarCommando(String cmd){
+        try {
+            Process proc = Runtime.getRuntime().exec(cmd);
+            BufferedInputStream buffer = new BufferedInputStream(
+                proc.getInputStream()
+            );
+            System.out.println(new String(buffer.readAllBytes()));
         } catch (Exception e) {
-            e.printStackTrace();
+            //TODO: handle exception
         }
     }
 
